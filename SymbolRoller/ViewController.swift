b05e6ae406d7d589338f01ea7c7d21b9127a0416
hongduhyeon <ghdengus12@naver.com>
2023-10-16 23:05:16 +0900

    Initial Commit

---
+++ SymbolRoller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SymbolRoller
+//
+//  Created by 홍두현 on 10/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
