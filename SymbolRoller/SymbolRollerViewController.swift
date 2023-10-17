496ccb06d6b2f1f1c6bb794e0485a4fb342b148d
hongduhyeon <ghdengus12@naver.com>
2023-10-17 23:16:17 +0900

    initial commit

---
+++ SymbolRoller/SymbolRollerViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SymbolRollerViewController.swift
+//  SymbolRoller
+//
+//  Created by 홍두현 on 10/16/23.
+//
+
+import UIKit
+
+class SymbolRollerViewController: UIViewController {
+
+    let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO:
+        // 심볼에서 하나를 임의로 추출해서 이미지와 텍스트를 설정한다.
+        reload()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    func reload() {
+        let symbol = symbols.randomElement()!
+        
+        imageView.image = UIImage(systemName: symbol)
+        label.text = symbol
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        reload()
+    }
+    
+}
